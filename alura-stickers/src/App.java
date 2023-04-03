0c2122886bdb63e84d753c5b87a4b74ffff7b074
Daniel <danielribas1105@gmail.com>
2023-04-03 12:08:20 -0300

    create extractors classes

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -11,35 +11,36 @@ public class App {
 
         //String imdbKey = System.getenv("IMDB_KEY");//capturar uma variável de ambiente
         //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
-        //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        String url = "https://api.nasa.gov/planetary/apod?api_key=1h6ReN05vxXQkBoJgP8huhYaQDvYmzqHpF0kdKCG&start_date=2023-04-01&end_date=2023-04-03";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        //String url = "https://api.nasa.gov/planetary/apod?api_key=1h6ReN05vxXQkBoJgP8huhYaQDvYmzqHpF0kdKCG&start_date=2023-04-01&end_date=2023-04-03";
 
         ClientHttp clientHttp = new ClientHttp();
         String jason = clientHttp.findDados(url);
-
-        //Extrair somente os dados que interessam(Título, Poster, Classificação)
-
-        var parser = new JsonParser();
-        List<Map<String, String>> contentList = parser.parse(jason);
 
         //Criando o diretório
         String pastaSaida = "stickers/";
         var path = new File(pastaSaida);
         path.mkdir();
 
+        //exibir e manipular os dados
+        //ContentExtractorNasa extractor = new ContentExtractorNasa();
+        ContentExtractorImdb extractor = new ContentExtractorImdb();
+        List<Content> contents = extractor.extractContent(jason);
+
         //Exibir os dados da forma que escolhermos
         var factory = new FactoryStickers();
-        for (Map<String,String> content : contentList) {
+        for (int i = 0; i < contents.size(); i++) {
 
-            String nomeArquivo = content.get("title") + ".png";
-            String urlImagem = content.get("url");
+            Content content = contents.get(i);
+            String nomeArquivo = content.getTitle() + ".png";
+            //String urlImagem = content.getUrlImage();
             //double notaFilme = Double.parseDouble(content.get("imDbRating"));
             double notaFilme = 9.0;
-            InputStream inputStream = new URL(urlImagem).openStream();
 
+            InputStream inputStream = new URL(content.getUrlImage()).openStream();
             factory.createSticker(inputStream, pastaSaida + nomeArquivo, notaFilme);
 
-            System.out.println("\u001b[1m" + "TÍTULO: " + "\u001b[36m" + "\u001b[3m" + content.get("title") + "\u001b[m");
+            System.out.println("\u001b[1m" + "TÍTULO: " + "\u001b[36m" + "\u001b[3m" + nomeArquivo + "\u001b[m");
             /*System.out.println("\u001b[1m" + "CARTAZ: " + "\u001b[m" + content.get("image"));
             System.out.println("\u001b[1m" + "CLASSIFICAÇÃO: " + "\u001b[34m" + content.get("imDbRating") + "\u001b[m");
 
---
+++ alura-stickers/src/ContentExtractorImdb.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ContentExtractorImdb {
+
+    public List<Content> extractContent(String json) {
+        var parser = new JsonParser();
+        List<Map<String, String>> atributList = parser.parse(json);
+        List<Content> contents = new ArrayList<>();
+        for (Map<String, String> atribut: atributList) {
+            String title = atribut.get("title");
+            String urlImage = atribut.get("image");
+            var content = new Content(title, urlImage);
+            contents.add(content);
+        }
+        return  contents;
+    }
+}
